7f2eaa3c1dc8407496d4926c6798e30042cc47fb
Alyssa Yates <bosslys@icloud.com>
2019-11-07 19:52:46 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.decimal "balance", precision: 5, scale: 1
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
