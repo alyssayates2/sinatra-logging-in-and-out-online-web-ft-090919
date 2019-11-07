03876d10af0b564e1216edb023a6f57839041daa
Alyssa Yates <bosslys@icloud.com>
2019-11-07 19:32:02 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
 
   def Helpers.current_user(session)
-    @user = User.find_by(session[:user_id])
+    @user = User.find_by(id: session[:user_id])
   end
 
   def Helpers.is_logged_in?(session)
